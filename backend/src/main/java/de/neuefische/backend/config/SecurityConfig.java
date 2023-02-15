040e7500d1314f6b4da7e25978f6e2adba346d3b
maikpopowicz <maik.popowicz@outlook.com>
2023-02-15 10:50:55 +0100

    fix: deployment

--- backend/src/main/java/de/neuefische/backend/config/SecurityConfig.java
+++ backend/src/main/java/de/neuefische/backend/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import java.util.Optional;
     @RequiredArgsConstructor
     public class SecurityConfig {
         private final AppUserService appUserService;
+
         @Generated("*")
         @Bean
         public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
